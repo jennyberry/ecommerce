ce2a06125f9bb7c4d00e1bc7723568ef61236c58
jennypham <64534681+jennyberry@users.noreply.github.com>
2022-11-02 19:40:04 +1100

    home page

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HeroBanner from "./HeroBanner";
+import Navbar from "./Navbar";
+import Layout from "./Layout";
+import Product from "./Product";
+import Footer from "./Footer";
+import FooterBanner from "./FooterBanner";
+import Cart from "./Cart";
+
+export { HeroBanner, Navbar, Layout, Product, Footer, FooterBanner, Cart };
